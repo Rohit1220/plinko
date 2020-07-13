e4250c11032edded21c2a5ae0f0e4553df0578f3
Rohit1220 <65952260+Rohit1220@users.noreply.github.com>
2020-07-13 13:42:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -32,20 +32,20 @@ function setup() {
      for (var j = 50; j <=width-10; j=j+50) 
     {
        plinkos.push(new Plinko(j,375));
-    }    
+    }   
 }
 function draw() {
   background("black");
   textSize(20)
  //text("Score : "+score,20,30);
   Engine.update(engine); 
-   for (var i = 0; i < plinkos.length; i++) {
-     plinkos[i].display();
-   }
-   if(frameCount%60===0){
-     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
-     score++;
-   }
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();
+  }
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+    score++;
+  } 
   for (var j = 0; j < particles.length; j++) {
      particles[j].display();
    }
